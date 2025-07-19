c1e535b2cbd839a5a76889b4a38ca4a19ca29af7
Miguel García Melgar <mikongame@gmail.com>
2025-07-19 18:18:43 +0200

    delete project

--- nodejs/routes/libro.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Ruta /libro/alta
-router.get('/alta', (req, res) => {
-  res.send('Formulario alta');
-});
-
-export default router;
